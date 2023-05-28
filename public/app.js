921242153695cbe78e2c71dc449adfdad162cb7d
ReadingTheRoom <readtheroom@outlook.com.au>
2023-05-28 18:22:01 +1000

    June reduced color palette and cleaned flex left

---
+++ public/app.js
@@ -0,0 +1,15 @@
+const mobilebooks = document.querySelectorAll(".mobilelayout")
+document.addEventListener('touchstart', mobilebooks, () => {
+  
+    .book - cover {
+    transform: rotateX(75deg) translateZ(3px) scale(0.75) translateY(-302px);
+  }
+    
+      .preface {
+    transform: translateY(-302px);
+  }
+    
+      .body {
+    visibility: visible;
+  }
+})
